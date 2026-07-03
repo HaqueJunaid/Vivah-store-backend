b07d75fe2ca977a9500a5d69cf61287e731e48b4
Junaid Haque <junaid132boss@gmail.com>
2026-07-03 04:10:26 -0700

    fix: update CORS origin to allow all origins

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+    origin: '*',
     credentials: true,
   })
 );
